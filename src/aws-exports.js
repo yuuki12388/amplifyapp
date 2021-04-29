3271ad3c5444befddd1a056c07573a8547a31b3f
Yuuki Cross <yuuki12388@gmail.com>
2021-04-29 22:28:34 +0800

    add graphql

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_sqYupfkdj",
     "aws_user_pools_web_client_id": "6naac6pltl9t901flmbh5cvqhs",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://wahwjwpghbda7fcsvoapp7javq.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-b3unkox2rbejngs6aiaawfboxu"
 };
 
 
